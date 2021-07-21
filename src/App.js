2e57c771cebbeddee236054340a4cefd16aba81b
sumedhbajracharya <sumedhbajracharya07@gmail.com>
2021-07-21 20:33:59 +0545

    Animations and improvements

--- src/App.js
+++ src/App.js
@@ -4,11 +4,13 @@ import BG_GRID from './assets/BG_GRID.png'
 import CustomCursor from 'custom-cursor-react';
 import 'custom-cursor-react/dist/index.css';
 import { useState } from 'react';
+import SUMEDH from './assets/SUMEDH.png'
 
 const App = () => {
   const [activeTab, setActiveTab] = useState(1);
   return (
-    <main>
+    <main
+      className={activeTab === 1 ? 'nm8-1' : activeTab === 2 ? 'nm8-2' : activeTab === 3 ? 'nm8-3' : activeTab === 4 ? 'nm8-4' : ''}>
       <section className="home flex relative">
         <div className="bg-grid flex-centered">
           <img src={BG_GRID} alt="BG_GRID" />
@@ -16,12 +18,12 @@ const App = () => {
         <div style={{ width: '20%' }} className="indicate-title secondary-text full-height flex column justify-between">
           <div className="text">
             <div className="fw-600">PORTFOLIO OF</div>
-            <div className="fw-800">SUMEDH<br /> BAJRACHARYA</div>
+            <div className="fw-800">SUMEDH<br /> <span className="fw-900">BAJRACHARYA</span></div>
           </div>
           {/* <ArrowDownCircle size={77} className="pointer" /> */}
         </div>
         <div className="container flex-centered full-height full-width relative transitionDiv">
-          <div className="bg-el-home"></div>
+          {/* <div className="bg-el-home"></div> */}
           {
             activeTab === 1 ?
               <div
@@ -33,14 +35,18 @@ const App = () => {
               </div>
               :
               activeTab === 2 ?
-                <div
-
-                  className="headline">
-                  I AM <br />
-                  FROM<br />
-                  <span className="bkt">
-                    <a target="_blank" rel="noreferrer" href="https://www.google.com/maps/place/Bhaktapur/data=!4m2!3m1!1s0x39eb1aae42806ba1:0x5449e079404e5e82?sa=X&ved=2ahUKEwjyxayZjPTxAhXf3jgGHZFnBYoQ8gEwAHoECAYQAQ">BHAKTAPUR
-                    </a></span>
+                <div className="about flex">
+                  <div
+                    className="headline">
+                    I AM <br />
+                    FROM<br />
+                    <span className="bkt">
+                      <a target="_blank" rel="noreferrer" href="https://www.google.com/maps/place/Bhaktapur/data=!4m2!3m1!1s0x39eb1aae42806ba1:0x5449e079404e5e82?sa=X&ved=2ahUKEwjyxayZjPTxAhXf3jgGHZFnBYoQ8gEwAHoECAYQAQ">BHAKTAPUR
+                      </a></span>
+                  </div>
+                  <img className="ml-xl" 
+                  style={{zIndex:'0'}} 
+                  src={SUMEDH} alt="SUMEDH" width="350px"/>
                 </div>
                 :
                 activeTab === 3 ?
@@ -67,19 +73,19 @@ const App = () => {
         </div>
         <div className="nav flex column items-end justify-end"
           style={{ fontSize: '1.4rem', width: '20%' }}>
-          <div className={activeTab === 1 ? 'active nav-item secondary-text fw-600 pt-md' : 'nav-item secondary-text fw-600 pt-md'}
+          <div className={activeTab === 1 ? 'active nav-item secondary-text fw-600 py-md' : 'nav-item secondary-text fw-600 py-md'}
             onClick={() => {
               setActiveTab(1)
             }}>HOME</div>
-          <div className={activeTab === 2 ? 'active nav-item secondary-text fw-600 pt-md' : 'nav-item secondary-text fw-600 pt-md'}
+          <div className={activeTab === 2 ? 'active nav-item secondary-text fw-600 py-md' : 'nav-item secondary-text fw-600 py-md'}
             onClick={() => {
               setActiveTab(2)
             }}>ABOUT</div>
-          <div className={activeTab === 3 ? 'active nav-item secondary-text fw-600 pt-md' : 'nav-item secondary-text fw-600 pt-md'}
+          <div className={activeTab === 3 ? 'active nav-item secondary-text fw-600 py-md' : 'nav-item secondary-text fw-600 py-md'}
             onClick={() => {
               setActiveTab(3)
             }}>WORK</div>
-          <div className={activeTab === 4 ? 'active nav-item secondary-text fw-600 pt-md' : 'nav-item secondary-text fw-600 pt-md'}
+          <div className={activeTab === 4 ? 'active nav-item secondary-text fw-600 py-md' : 'nav-item secondary-text fw-600 py-md'}
             onClick={() => {
               setActiveTab(4)
             }}>CONTACT</div>
